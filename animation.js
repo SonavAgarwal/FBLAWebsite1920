759b5e46c764f49d581f9f925df83410e8d8517b
Wyatt Cowley <wycowley@gmail.com>
2019-12-03 15:26:57 -0800

    opacity changing transition
    slideshow using opacity, with no external controls

--- animation.js
+++ animation.js
@@ -1,4 +1,6 @@
+
 function destroy() {
+    setInterval(change,3000);
     console.log("destroying");
     setTimeout(kaBoom, 980);
 }
@@ -6,4 +8,23 @@ function destroy() {
 function kaBoom() {
     document.getElementById("intro").style.display = "none";
     console.log("destroyed")
+}
+
+
+function change(){
+    if(document.getElementById("slideshow3").className == "appear"){
+        console.log("test");
+        document.getElementById("slideshow3").className = "fade";
+        document.getElementById("slideshow1").className = "appear";
+    }else if(document.getElementById("slideshow2").className == "appear"){
+        console.log("test");
+
+        document.getElementById("slideshow2").className = "fade";
+        document.getElementById("slideshow3").className = "appear";
+    }else if(document.getElementById("slideshow1").className == "appear"){
+        console.log("test");
+
+        document.getElementById("slideshow1").className = "fade";
+        document.getElementById("slideshow2").className = "appear";
+    }
 }
